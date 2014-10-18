86c102a9502c9869bfd061ee487fdf415dc4646d
Dunya Kirkali <dunyakirkali@gmail.com>
2014-10-18 17:49:02 +0200

    using kumaception to clean up code

--- spec/kuma_spec.rb
+++ spec/kuma_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Kuma" do
-  describe "MD templates" do
-    it "should emit valid MD to STDOUT" do
-      run_simple "kuma"
+describe 'Kuma' do
+  describe 'MD templates' do
+    it 'should emit valid MD to STDOUT' do
+      run_simple 'kuma'
       assert_exit_status(0)
     end
   end
